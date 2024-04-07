c5d884ab829c5141a2c9db4e4d19744432cd370a
alanmath <alanmatheus187@gmail.com>
2024-04-07 18:13:41 -0300

    swagger

--- src/main/java/insper/collie/service/MicroserviceController.java
+++ src/main/java/insper/collie/service/MicroserviceController.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "collie-service")
+@FeignClient(name = "collie-microservices")
 public interface MicroserviceController {
     
     @PostMapping("/microservices")
